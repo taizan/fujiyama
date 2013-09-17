9e4893097ababbbf3f6d0001d019d198a326db6d
Taizan Yonetsuji <ecomaki@ecomaki.com>
2013-09-17 23:20:52 +0900

    make basic game bone

--- game.js
+++ game.js
@@ -1,5 +1,16 @@
 Game= function(){
     game = this;
+    loop = this.loop;
+    setInterval(loop, 1000);
+    b = [];
+    b[0] = new building($(".menu li")[0], 1, 10);
+    b[1] = new building($(".menu li")[1], 2, 100);
+    b[2] = new building($(".menu li")[2], 5, 500);
+    b[3] = new building($(".menu li")[3], 10, 1000);
+    b[4] = new building($(".menu li")[4], 20, 3000);
+    b[5] = new building($(".menu li")[5], 40, 10000);
+    b[6] = new building($(".menu li")[6], 100, 100000);
+    
 }
 
 Game.prototype = {
@@ -10,11 +21,13 @@ Game.prototype = {
 
     visited: 0,
 
-    voltage: 200,
+    voltage: 20,
+
+    vol_time:10,
+
 
     update: function () {
-        this.visiting += this.visit_ps;
-        this.visited += this.visit_ps;
+        game.add_visiter(game.visit_ps);
     },
 
     set_visiter: function () {
@@ -30,9 +43,60 @@ Game.prototype = {
     },
 
     add_visiter : function (n) {
-        this.visited += n;
-        this.visiting += n;
-        $("#num").html(this.visiting + "人、ご来訪");
-        $("#red_gage").css({height: 100*this.visiting/this.voltage +"%"})
+        game.visited += n;
+        game.visiting += n;
+        $("#num").html(game.visiting + "人、ご来訪");
+        $("#ps").html(game.visit_ps + "人/日(s)");
+        
+        if (this.visiting > this.voltage) {
+            game.hunka();
+            game.visiting = 0;
+            game.voltage *= 2;
+        }
+        $("#red_gage").css({ height: 100 * game.visiting / game.voltage + "%" })
+    },
+
+    hunka : function(){
+        $("#hunka").show("bounce", {}, "slow");
+        $("#red_gage img").hide("explode", {}, "nomale", function () { $("#red_gage img").show(); });
+        game.volting = game.vol_time;
+    },
+
+    loop: function () {
+        game.update();
+        if (game.volting > 0) {
+            game.volting -= 1;
+        } else if(game.volting == 0){
+            $("#hunka").hide("slow");
+            game.volting -= 1;
+        }
+        console.log( game.volting);
     }
-}
+
+    
+};
+
+
+building = function($el,pt,price){
+    this.el = $el;
+    this.pt = pt;
+    this.price = price;
+    this.n = 0;
+    var self = this;
+
+    $(".price" , self.el).html(self.price+"人力");
+    $(".num" , self.el).html(self.n);
+
+    $(this.el).click(function(){
+        if(game.visiting >= self.price ){
+            game.visiting -= self.price;
+            self.n+=1;
+            game.visit_ps += self.pt;
+            $(".price" , self.el).html(self.price+"人力");
+            $(".num" , self.el).html(self.n);
+        }
+    });
+
+};
+
+
